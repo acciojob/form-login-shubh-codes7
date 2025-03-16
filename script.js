866924c9fddc7610983a72cd2cad204b47e38532
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-16 19:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
+
+		let fname = document.getElementsByName("fname")[0].value;
+		let lname = document.getElementsByName("lname")[0].value;
+		alert(fname + " " + lname)
 
 }
